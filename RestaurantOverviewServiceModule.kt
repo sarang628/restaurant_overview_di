c57cade3b2f528a2e7cf186d131fab45ff52ebba
sryang <sarang628@gmail.com>
2025-07-09 13:11:46 +0800

    update

--- RestaurantOverviewServiceModule.kt
+++ RestaurantOverviewServiceModule.kt
@@ -1,7 +1,12 @@
 package com.sarang.torang.di.restaurant_overview_di
 
 import com.sarang.library.data.Feed
+import com.sarang.library.data.MenuData
+import com.sarang.library.data.RestaurantImage
 import com.sarang.library.usecase.FetchReviewsUseCase
+import com.sarang.library.usecase.GetMenuUseCase
+import com.sarang.library.usecase.GetRestaurantGalleryUseCase
+import com.sarang.torang.api.ApiRestaurant
 import com.sarang.torang.api.ApiReview
 import com.sarang.torang.api.handle
 import dagger.Module
@@ -22,6 +27,26 @@ class RestaurantOverviewServiceModule {
                 } catch (e: HttpException) {
                     throw Exception(e.handle())
                 }
+            }
+        }
+    }
+
+    @Provides
+    fun providesGetRestaurantGalleryUseCase(
+        apiRestaurant: ApiRestaurant,
+    ): GetRestaurantGalleryUseCase {
+        return object : GetRestaurantGalleryUseCase {
+            override suspend fun invoke(restaurantId: Int): List<RestaurantImage> {
+                return apiRestaurant.getRestaurantDetail(restaurantId).toRestaurantImages()
+            }
+        }
+    }
+
+    @Provides
+    fun providesGetMenuUseCase(apiRestaurant: ApiRestaurant): GetMenuUseCase {
+        return object : GetMenuUseCase {
+            override suspend fun invoke(restaurantId: Int): List<MenuData> {
+                return apiRestaurant.getRestaurantDetail(restaurantId).toMenus()
             }
         }
     }

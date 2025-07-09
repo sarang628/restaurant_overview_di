534e31c738a1b483bbe493ce4fba6defaa3b1f25
sryang <sarang628@gmail.com>
2025-07-09 13:07:45 +0800

    update

---
+++ RestaurantOverviewServiceModule.kt
@@ -0,0 +1,29 @@
+package com.sarang.torang.di.restaurant_overview_di
+
+import com.sarang.library.data.Feed
+import com.sarang.library.usecase.FetchReviewsUseCase
+import com.sarang.torang.api.ApiReview
+import com.sarang.torang.api.handle
+import com.sarang.torang.di.restaurant_detail_di.toFeedData
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.HttpException
+
+@InstallIn(SingletonComponent::class)
+@Module
+class RestaurantOverviewServiceModule {
+    @Provides
+    fun providesFetchReviewsUseCase(apiReview: ApiReview): FetchReviewsUseCase {
+        return object : FetchReviewsUseCase {
+            override suspend fun invoke(restaurantId: Int): List<Feed> {
+                try {
+                    return apiReview.getReviews(restaurantId).map { it.toFeedData() }
+                } catch (e: HttpException) {
+                    throw Exception(e.handle())
+                }
+            }
+        }
+    }
+}

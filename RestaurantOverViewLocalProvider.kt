654b65d05dc0f6a9e5dfb54066db625b3864ad29
sryang <sarang628@gmail.com>
2025-09-04 17:36:17 +0800

    set onLocation event

--- RestaurantOverViewLocalProvider.kt
+++ RestaurantOverViewLocalProvider.kt
@@ -3,21 +3,21 @@ package com.sarang.torang.di.restaurant_overview_di
 import androidx.compose.runtime.CompositionLocalProvider
 import com.sarang.torang.LocalRestaurantInfoImageLoader
 import com.sarang.torang.RestaurantInfoImageLoader
+import com.sarang.torang.RootNavController
 import com.sarang.torang.compose.type.RestaurantOverViewImageLoader
 import com.sarang.torang.compose.type.RestaurantOverviewRestaurantInfo
 import com.sarang.torang.di.image.provideTorangAsyncImage
-import com.sarang.torang.di.restaurant_info.RestaurantInfoWithPermission
 import com.sarang.torang.di.restaurant_info.RestaurantInfoWithPermissionWithLocation
-import com.sryang.library.compose.workflow.BestPracticeViewModel
 
 val restaurantOverViewImageLoader: RestaurantOverViewImageLoader = { modifier, url, width, height, scale ->
     // 여기서 실제 이미지 로딩 구현 예시
     provideTorangAsyncImage().invoke(modifier, url, width, height, scale)
 }
 
-val restaurantOverViewRestaurantInfo: RestaurantOverviewRestaurantInfo = {
+fun restaurantOverViewRestaurantInfo(rootNavController: RootNavController): RestaurantOverviewRestaurantInfo = {
     CompositionLocalProvider(LocalRestaurantInfoImageLoader provides restaurantInfoImageLoader){
-        RestaurantInfoWithPermissionWithLocation(restaurantId = it)
+        RestaurantInfoWithPermissionWithLocation(restaurantId = it,
+            onLocation = { rootNavController.map() })
     }
 }
 

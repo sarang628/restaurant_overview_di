feb493a9ff2717233e937677d45b6b85cdb5c2f5
sryang <sarang628@gmail.com>
2025-09-08 18:23:55 +0800

    change restaurantInfo and add RequiresPermission

--- RestaurantOverViewLocalProvider.kt
+++ RestaurantOverViewLocalProvider.kt
@@ -1,23 +1,27 @@
 package com.sarang.torang.di.restaurant_overview_di
 
+import androidx.annotation.RequiresPermission
 import androidx.compose.runtime.CompositionLocalProvider
 import com.sarang.torang.LocalRestaurantInfoImageLoader
 import com.sarang.torang.RestaurantInfoImageLoader
+import com.sarang.torang.RestaurantInfoViewModel
 import com.sarang.torang.RootNavController
 import com.sarang.torang.compose.type.RestaurantOverViewImageLoader
 import com.sarang.torang.compose.type.RestaurantOverviewRestaurantInfo
 import com.sarang.torang.di.image.provideTorangAsyncImage
-import com.sarang.torang.di.restaurant_info.RestaurantInfoWithPermissionWithLocation
+import com.sarang.torang.di.restauarnt_info_di.restaurantInfo
 
 val restaurantOverViewImageLoader: RestaurantOverViewImageLoader = { modifier, url, width, height, scale ->
     // 여기서 실제 이미지 로딩 구현 예시
     provideTorangAsyncImage().invoke(modifier, url, width, height, scale)
 }
 
-fun restaurantOverViewRestaurantInfo(rootNavController: RootNavController): RestaurantOverviewRestaurantInfo = {
+@RequiresPermission(anyOf = ["android.permission.ACCESS_COARSE_LOCATION", "android.permission.ACCESS_FINE_LOCATION"])
+fun restaurantOverViewRestaurantInfo(rootNavController: RootNavController, viewModel : RestaurantInfoViewModel): RestaurantOverviewRestaurantInfo = {
     CompositionLocalProvider(LocalRestaurantInfoImageLoader provides restaurantInfoImageLoader){
-        RestaurantInfoWithPermissionWithLocation(restaurantId = it,
-            onLocation = { rootNavController.map(it) })
+        restaurantInfo(viewModel = viewModel).invoke(it, {
+            rootNavController.map(it)
+        }, {}, {})
     }
 }
 

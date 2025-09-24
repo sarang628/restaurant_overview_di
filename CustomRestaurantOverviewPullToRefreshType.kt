79b68da6d00dbb299f9ee6d02e5021181cae4162
sryang <sarang628@gmail.com>
2025-09-24 15:05:26 +0800

    update pull to refresh

--- CustomRestaurantOverviewPullToRefreshType.kt
+++ CustomRestaurantOverviewPullToRefreshType.kt
@@ -1,9 +1,12 @@
 package com.sarang.torang.di.restaurant_overview_di
 
 import com.sarang.torang.compose.type.PullToRefresh
+import com.sarang.torang.di.pulltorefresh.PullToRefreshData
 import com.sarang.torang.di.pulltorefresh.providePullToRefresh
 import com.sryang.library.pullrefresh.rememberPullToRefreshState
 
 val CustomRestaurantOverviewPullToRefreshType: PullToRefresh = { modifier, isRefreshing, onRefresh, contents ->
-    providePullToRefresh(rememberPullToRefreshState()).invoke(modifier, isRefreshing, onRefresh, contents)
+    providePullToRefresh(rememberPullToRefreshState()).invoke(
+        PullToRefreshData(modifier, com.sarang.torang.compose.feed.state.RefreshIndicatorState.Default , onRefresh, contents)
+    )
 }

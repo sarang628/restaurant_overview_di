f15d57bee8638368ce1ab48aab32f23d0e3344b9
sryang <sarang628@gmail.com>
2025-11-19 14:24:36 +0900

    add ProvideRestaurantOverivew

---
+++ RestaurantOverivewProvider.kt
@@ -0,0 +1,39 @@
+package com.sarang.torang.di.restaurant_overview_di
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.sarang.torang.RestaurantInfoViewModel
+import com.sarang.torang.RootNavController
+import com.sarang.torang.compose.feed.internal.components.LocalExpandableTextType
+import com.sarang.torang.compose.feed.internal.components.LocalFeedImageLoader
+import com.sarang.torang.compose.restaurantdetail.RestaurantOverViewScreen
+import com.sarang.torang.compose.restaurantdetail.feed.LocalRestaurantFeed
+import com.sarang.torang.compose.type.LocalPullToRefresh
+import com.sarang.torang.compose.type.LocalRestaurantOverViewImageLoader
+import com.sarang.torang.compose.type.LocalRestaurantOverviewRestaurantInfo
+import com.sarang.torang.di.basefeed_di.CustomExpandableTextType
+import com.sarang.torang.di.basefeed_di.CustomFeedImageLoader
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProvideRestaurantOverivew(restaurantId : Int){
+    val viewModel : RestaurantInfoViewModel = hiltViewModel()
+    CompositionLocalProvider(
+        LocalRestaurantOverViewImageLoader    provides restaurantOverViewImageLoader,
+        LocalRestaurantOverviewRestaurantInfo provides restaurantOverViewRestaurantInfo(
+            rootNavController = RootNavController(), viewModel = viewModel),
+        LocalRestaurantFeed                   provides CustomRestaurantFeedType,
+        LocalExpandableTextType               provides CustomExpandableTextType,
+        LocalFeedImageLoader                  provides CustomFeedImageLoader(),
+        LocalPullToRefresh                    provides CustomRestaurantOverviewPullToRefreshType
+    ) {
+        Box(Modifier.fillMaxSize()){
+            RestaurantOverViewScreen(restaurantId = restaurantId, onRefresh = {viewModel.refresh(1)}, isRefreshRestaurantInfo = viewModel.isRefresh)
+        }
+    }
+}

b5a5668ccbfcbdf1f68a267202423e07b9b40d68
sryang <sarang628@gmail.com>
2026-03-14 12:57:19 +0900

    update modules

--- CustomRestaurantOverviewPullToRefreshType.kt
+++ CustomRestaurantOverviewPullToRefreshType.kt
@@ -3,10 +3,11 @@ package com.sarang.torang.di.restaurant_overview_di
 import com.sarang.torang.compose.type.PullToRefresh
 import com.sarang.torang.di.pulltorefresh.PullToRefreshData
 import com.sarang.torang.di.pulltorefresh.providePullToRefresh
+import com.sryang.library.pullrefresh.RefreshIndicatorState
 import com.sryang.library.pullrefresh.rememberPullToRefreshState
 
 val CustomRestaurantOverviewPullToRefreshType: PullToRefresh = { modifier, isRefreshing, onRefresh, contents ->
     providePullToRefresh(rememberPullToRefreshState()).invoke(
-        PullToRefreshData(modifier, com.sarang.torang.compose.feed.state.RefreshIndicatorState.Default , onRefresh, contents)
+        PullToRefreshData(modifier, RefreshIndicatorState.Default , onRefresh, contents)
     )
 }
--- DataConverter.kt
+++ DataConverter.kt
@@ -13,9 +13,9 @@ import com.sarang.torang.data.remote.response.RestaurantDetailApiModel
 fun RestaurantDetailApiModel.toMenus(): List<MenuData> {
     return this.menus.map {
         MenuData(
-            menuName = it.menu_name ?: "",
-            price = it.menu_price?.toFloat() ?: 0f,
-            url = BuildConfig.MENU_IMAGE_SERVER_URL + it.menu_pic_url
+            menuName = it.menuName ?: "",
+            price = it.menuPrice?.toFloat() ?: 0f,
+            url = BuildConfig.MENU_IMAGE_SERVER_URL + it.menuPicUrl
         )
     }
 }

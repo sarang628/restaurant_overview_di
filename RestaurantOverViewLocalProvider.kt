2e3d8cf7dfddbfcbd990f346ced32c1e9caf4f61
sryang <sarang628@gmail.com>
2026-03-16 12:27:26 +0900

    update modules

--- RestaurantOverViewLocalProvider.kt
+++ RestaurantOverViewLocalProvider.kt
@@ -1,15 +1,13 @@
 package com.sarang.torang.di.restaurant_overview_di
 
-import androidx.annotation.RequiresPermission
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.sarang.torang.LocalRestaurantInfoImageLoader
-import com.sarang.torang.RestaurantInfoData
 import com.sarang.torang.RestaurantInfoImageLoader
-import com.sarang.torang.RestaurantInfoScreenData
 import com.sarang.torang.RestaurantInfoViewModel
 import com.sarang.torang.RootNavController
+import com.sarang.torang.compose.restaurantinfo.RestaurantInfoScreenData
 import com.sarang.torang.compose.type.RestaurantOverViewImageLoader
 import com.sarang.torang.compose.type.RestaurantOverviewRestaurantInfo
 import com.sarang.torang.di.image.TorangAsyncImageData
